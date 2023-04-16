36b691c4f0d109f4b2fb1cbd5b11a1d627106c08
Peikun Chen <jackpeikun@gmail.com>
2023-04-16 04:20:31 -0400

    refactor backend

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const { Configuration, OpenAIApi } = require('openai')
+const cors = require('cors')
+const { performance } = require('perf_hooks')
+const { chatModes, accountConfigs } = require('./configs/configs')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const configuration = new Configuration({
+  organization: 'org-NB2fABjUp7msXUadrCdWDtv5',
+  apiKey: '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',
+})
+const openai = new OpenAIApi(configuration)
+
+// ************************
+// ******  REST API *******
+// ************************
+app.post('/api/chat', async (req, res) => {
+  try {
+    const userInput = req.body.userInput.trim()
+    console.log('User Input: [%s]', userInput)
+
+    if (userInput === 'Test') {
+      await new Promise((resolve) => setTimeout(resolve, 3000))
+      res.json(
+        'Test 疯狂测试疯狂测试疯狂测试疯狂测试疯狂测试疯狂测试疯狂测试疯狂测试疯狂测试疯狂测试疯狂测试疯狂测试'
+      )
+      return
+    }
+    const sessionMessages = [
+      ...req.body.sessionMessages,
+      { role: 'user', content: userInput },
+    ]
+
+    const startTime = performance.now()
+
+    console.debug('sessionMessages: [%s]', JSON.stringify(sessionMessages))
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: sessionMessages,
+      temperature: 0.9,
+    })
+    console.log(response.data.choices)
+    res.json(response.data.choices[0].message.content)
+
+    const endTime = performance.now()
+    const responseTime = endTime - startTime
+    console.log(`API response time: ${responseTime.toFixed(2)} ms`)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error occurred while processing request.')
+  }
+})
+
+app.get('/api/chat-modes', (req, res) => {
+  res.status(200).json(chatModes)
+})
+
+app.get('/api/accountConfigs', (req, res) => {
+  res.status(200).json(accountConfigs)
+})
+
+const PORT = process.env.PORT || 5001
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
