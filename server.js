4e7721e64ded82bf31e2ff9cd03b3d5de59cbb4f
Peikun Chen <jackpeikun@gmail.com>
2023-04-19 09:17:02 -0400

    add logging

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const prompts = require("./prompts/prompts");
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  req.setEncoding("utf8");
+  req.rawBody = "";
+  req.on("data", (chunk) => {
+    req.rawBody += chunk;
+  });
+  req.on("end", () => {
+    console.log("Raw request body:", req.rawBody);
+    next();
+  });
+});
+
 app.use(express.json());
 
 const configuration = new Configuration({
